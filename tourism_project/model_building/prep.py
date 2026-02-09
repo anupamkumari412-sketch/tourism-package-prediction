f81a83e1011cec613b3041e968f057088875b795
anupamkumari412-sketch <anupamkumari412@gmail.com>
2026-02-09 09:57:59 +0530

    Update prep.py

--- tourism_project/model_building/prep.py
+++ tourism_project/model_building/prep.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import LabelEncoder
 from huggingface_hub import login, HfApi
 
 # Define constants for the dataset and output paths
-api = HfApi(token=os.getenv("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"))
+api = HfApi(token=os.getenv("HF_TOKEN"))
 DATASET_PATH = "hf://datasets/anupam-roy123/tourism-package-prediction/tourism.csv"
 df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
