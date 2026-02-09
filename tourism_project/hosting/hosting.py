79a76e6153a493b2024f913fa4a82719f1c59ae1
anupamkumari412-sketch <anupamkumari412@gmail.com>
2026-02-09 10:00:43 +0530

    Update hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -1,7 +1,7 @@
 from huggingface_hub import HfApi
 import os
 
-api = HfApi(token=os.getenv("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"))
+api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
     repo_id="anupam-roy123/tourism-package-prediction",          # the target repo
