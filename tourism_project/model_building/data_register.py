b82713038e6f465ad3992aa379c3ff0bcea9eeb7
anupamkumari412-sketch <anupamkumari412@gmail.com>
2026-02-09 09:57:31 +0530

    Update data_register.py

--- tourism_project/model_building/data_register.py
+++ tourism_project/model_building/data_register.py
@@ -7,7 +7,7 @@ repo_id = "anupam-roy123/tourism-package-prediction"
 repo_type = "dataset"
 
 # Initialize API client
-api = HfApi(token=os.getenv("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"))
+api = HfApi(token=os.getenv("HF_TOKEN"))
 
 # Step 1: Check if the space exists
 try:
